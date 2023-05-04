ab76a748f10edba1685974bef41f0c990eb8f823
Carl Igbanugo <carlchiagozie@gmail.com>
2023-05-04 05:08:53 -0700

    new update

--- lib/Splashscreen.dart
+++ lib/Splashscreen.dart
@@ -31,7 +31,7 @@ class _SplashWorkState extends State<SplashWork> {
         height: double.infinity,
         width: double.infinity,
         //padding: EdgeInsets.all(20),
-        // margin: EdgeInsets.all(20),
+        margin: EdgeInsets.all(20),
         color: (Color.fromARGB(255, 94, 91, 91)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
